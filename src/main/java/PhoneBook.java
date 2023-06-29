fad758390221d37b0ee75ee9c8377b4d12e59121
Kristina Bazaley <atra.nox.ch@gmail.com>
2023-06-29 13:48:22 +0700

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,23 @@
+import java.util.HashMap;
+
 public class PhoneBook {
+
+    private int counter;
+    HashMap<String, Integer> namesMap;
+    HashMap<String, Integer> numbersMap;
+
+    public PhoneBook() {
+        namesMap = new HashMap<>();
+        numbersMap = new HashMap<>();
+        counter = 0;
+    }
+
     public int add(String name, String number) {
-        return 0;
+        if (!namesMap.containsKey(name)) {
+            counter++;
+            namesMap.put(name, counter);
+            numbersMap.put(number, counter);
+        }
+        return namesMap.size();
     }
 }
